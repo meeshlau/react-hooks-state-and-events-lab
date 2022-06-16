69bcacf52c9584c4b37bcc80ad7c5de972c39e10
Michelle Lau <meeshklau@gmail.com>
2022-06-16 13:07:31 -1000

    commit tree 6b54b5a7157fffd97f7cbd8a8e24265b64ad074e

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-
+  const [isDark, setIsDark] = useState(false)
+  const dark = () => {
+    setIsDark(!isDark)
+  }
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const appClass = isDark ? "App dark" : "App light"
+  console.log(appClass)
 
   return (
-    <div className={appClass}>
+    <div className={isDark ? "App dark" : "App light"}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={dark}>{isDark ? "Light Mode" : "Dark Mode"}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
+    
   );
 }
 
